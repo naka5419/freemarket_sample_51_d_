2d26494d0f036219f80906870231ecbe20b9f906
katsulau <efthnyb.1994@gmail.com>
2019-05-29 14:38:46 +0900

    set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :products, only: [:index, :show, :new] do
+    collection do
+      get 'buy'
+    end
+  end
   root 'products#index'
-  resources :products, only: [:index, :show]
-  
 end
