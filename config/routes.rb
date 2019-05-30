72a8e644a4bf4c953faa756df5bbdfc99750c80f
katsulau <efthnyb.1994@gmail.com>
2019-05-30 16:19:10 +0900

    Define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:new, :index]
   resources :products, only: [:index, :show, :new] do
     collection do
       get 'buy'
