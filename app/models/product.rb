bf577282b78d3ae4482c2f314cbf01d1679b4f63
katsulau <efthnyb.1994@gmail.com>
2019-05-30 13:28:20 +0900

    modify association and define enum shipping_cost

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_many :comments
   has_many :product_categories
-  has_many :categories, through: :product-categories
+  has_many :categories, through: :product_categories
   has_many :trade_messages
   has_many :likes
   belongs_to :bland
@@ -9,4 +9,7 @@ class Product < ApplicationRecord
   belongs_to :seller, class_name: "User"
   belongs_to :size
   has_many :evaluations
+  has_many_attached :images
+
+  enum shipping_cost: { "送料込み（出品者負担）": "0", "着払い（購入者負担）": "1" }
 end
