ba30587867c6156db4cdeb7bbe447c0ff27922b1
RinMomii <rinmomii@gmail.com>
2019-05-31 21:22:07 +0900

    Revert "Revert "Merge branch 'master' into topPageMarkup""
    This reverts commit bf09dd2adda9d981f82b1016994e99d74eb3307f.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:new, :index]
+  resources :products, only: [:index, :show, :new] do
+    collection do
+      get 'buy'
+    end
+  end
   root 'products#index'
 end
