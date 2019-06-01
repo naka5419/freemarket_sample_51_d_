1d62037f3a7ba9856b9ba46659542d1fe65ad235
katsulau <efthnyb.1994@gmail.com>
2019-06-01 17:20:51 +0900

    Profilesコントローラのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:new, :index]
+  resources :users, only: [:new, :index] do
+    resources :profiles do
+      collection do
+        get 'edit_address'
+      end
+    end
+  end
   resources :products, only: [:index, :show, :new] do
     collection do
       get 'buy'
