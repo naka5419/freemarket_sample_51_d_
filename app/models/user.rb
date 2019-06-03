045096a00d71b719c7b3f5397cb865146bb1b0c4
katsulau <efthnyb.1994@gmail.com>
2019-06-03 12:33:10 +0900

    ユーザーモデルにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 6 }
+  has_many :buyed_products, foreign_key: "buyer_id", class_name: "Product"
+  has_many :selling_products, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Product"
+  has_many :sold_products, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Product"
 
 end
