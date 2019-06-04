c3f8941bc49800e9725fcb4a773617a262371978
tanison <tanison949@icloud.com>
2019-06-04 18:46:06 +0900

    singup-view-ver1.0
    修正前、保存用

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
 
   devise_scope :user do
-    # get 'login' => 'users/sing_up/sms', as: :new_user_session
     get '/users/sign_up/sms' => 'users#sms', as: :users_registration_sms
     get '/users/sign_up/address' => 'users#address', as: :users_registration_address
     get '/users/sign_up/payment' => 'users#payment', as: :users_registration_payment
     get '/users/sign_up/close' => 'users#close', as: :users_registration_close
-    # get 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
   end
 end
