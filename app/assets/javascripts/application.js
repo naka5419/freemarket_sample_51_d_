8279371fbb9866444861a0c7a955d0834545b628
hgoya <z51.hidekazu.goya@gmail.com>
2019-06-04 15:56:28 +0900

    ビューの大枠

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery
 //= require activestorage
 //= require turbolinks
+//= require dropzone
 //= require_tree .
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_many :comments
   has_many :product_categories
-  has_many :categories, through: :product-categories
+  has_many :categories, through: :product_categories
   has_many :trade_messages
   has_many :likes
   belongs_to :bland
