281651bcbf60de754193b0f432fa6eef88e58df6
katsulau <efthnyb.1994@gmail.com>
2019-06-05 20:45:12 +0900

    外部キーにoptional: trueを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,10 @@ class Product < ApplicationRecord
   has_many :categories, through: :product_categories
   has_many :trade_messages
   has_many :likes
-  belongs_to :bland
-  belongs_to :buyer, class_name: "User"
-  belongs_to :seller, class_name: "User"
-  belongs_to :size
+  belongs_to :bland, optional: true
+  belongs_to :buyer, class_name: "User", optional: true
+  belongs_to :seller, class_name: "User", optional: true
+  belongs_to :size, optional: true
   has_many :evaluations
   has_many_attached :images
   enum condition: { 新品、未使用: 0, 未使用に近い: 1, 目立った傷や汚れなし: 2, やや傷や汚れあり: 3, 傷や汚れあり: 4, 全体的に状態が悪い: 5 }
