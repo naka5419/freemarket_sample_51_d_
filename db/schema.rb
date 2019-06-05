b759ae8ce600dc90ceb97a0e69a7c45ab67b33a3
tanison <tanison949@icloud.com>
2019-06-05 21:35:15 +0900

    signup-view-ver1.3
    指摘箇所の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_034931) do
+ActiveRecord::Schema.define(version: 2019_06_05_110314) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -99,6 +99,14 @@ ActiveRecord::Schema.define(version: 2019_06_05_034931) do
     t.string "firstname_kana"
     t.string "lastname_kana"
     t.integer "birthday"
+    t.integer "postalcode"
+    t.string "prefecture"
+    t.string "city_name"
+    t.string "address_number"
+    t.string "building_name"
+    t.integer "phone_number"
+    t.string "user_image"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
