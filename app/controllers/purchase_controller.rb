a7ee78ddd3ef2a901faa89f016da0f467a60d2b1
RinMomii <rinmomii@gmail.com>
2019-06-05 13:34:46 +0900

    Update purchase_controller.rb

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -7,13 +7,16 @@ class PurchaseController < ApplicationController
 
   def pay
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-    Payjp::Charge.create(
-    amount: 15500,
-    customer: card.customer_id,
-    currency: 'jpy'
-  )
-  redirect_to action: 'index'
+    if card.blank?
+      redirect_to action: "show"
+    else
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp::Charge.create(
+      amount: 15500,
+      customer: card.customer_id,
+      currency: 'jpy'
+    )
+    redirect_to action: 'index'
+    end
   end
-
 end
