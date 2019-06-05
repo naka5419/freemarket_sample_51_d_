ebdacb3301a4abc4a3c862fffb36a3aa1ec33b8d
RinMomii <rinmomii@gmail.com>
2019-06-05 10:56:55 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,16 @@ class ProductsController < ApplicationController
   end
 
   def new
-  end  
-  
+  end
+
   def show
   end
 
   def buy
+    card = Card.where(user_id: current_user.id).first
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
   end
 
 end
