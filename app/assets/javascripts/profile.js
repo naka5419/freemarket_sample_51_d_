aa4c156909fe21692bb45863ad34de5c4daf2ac1
naka5419 <hnakai323@yahoo.co.jp>
2019-06-07 11:43:16 +0900

    add-profile-js

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,16 @@
+$(function() {
+  $(".upper-tab__label").on("click",function(){
+    var $th = $(this).index();
+    $(".upper-tab__label").removeClass("active");
+    $(".upper-content__pane").removeClass("active");
+    $(this).addClass("active");
+    $(".upper-content__pane").eq($th).addClass("active");
+  });
+  $(".bottom-tab__label").on("click",function(){
+    var $th = $(this).index();
+    $(".bottom-tab__label").removeClass("active");
+    $(".bottom-content__pane").removeClass("active");
+    $(this).addClass("active");
+    $(".bottom-content__pane").eq($th).addClass("active");
+  });
+});
