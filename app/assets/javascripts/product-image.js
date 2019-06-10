5e294f3ab3b43dc7ba6e3a3c3bdc88f4781c6e23
hgoya <z51.hidekazu.goya@gmail.com>
2019-06-10 16:48:15 +0900

    画像投稿機能(jsファイル1)

--- app/assets/javascripts/product-image.js
+++ app/assets/javascripts/product-image.js
@@ -1,3 +1,7 @@
 $(document).on('turbolinks:load', function() {
   let image_field = $("#image_field");
+  
+  $("#upload_field").on('change', 'input[type="file"]', function(e) {
+    let file = e.target.files[0],
+  })
 });
