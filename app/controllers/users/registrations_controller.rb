2da7d587765583ac3334c38f128ebbd5d2b05f53
tanison <tanison949@icloud.com>
2019-06-11 20:12:16 +0900

    facebook-ver1.0
    facebookによるログイン

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -63,4 +63,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     new_smse_path
   end
+
+  def create
+    #binding.pry
+    if params[:user][:password] == "" #sns登録なら
+      params[:user][:password] = "Devise.friendly_token.first(6)" #deviseのパスワード自動生成機能を使用
+      params[:user][:password_confirmation] = "Devise.friendly_token.first(6)"
+      super
+      # binding.pry
+      sns = SnsCredential.update(user_id: @user.id)
+    else #email登録なら
+      # binding.pry
+      super
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module FreemarketSample51D
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    I18n.enforce_available_locales = false
+    config.i18n.default_locale = :ja
   end
 end
