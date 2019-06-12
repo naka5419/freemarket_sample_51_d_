9290e036b57f562b19b1bd4af060c56e6a84a649
RinMomii <rinmomii@gmail.com>
2019-06-12 13:31:30 +0900

    Update payments_controller.rb

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,4 +4,18 @@ class PaymentsController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "purchase/show"
+    else
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp::Charge.create(
+      amount: @product.price,
+      customer: card.customer_id,
+      currency: 'jpy'
+    )
+    end
+  end
 end
