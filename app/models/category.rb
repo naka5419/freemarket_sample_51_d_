d07879016797cf78d95b7e4f40787c9fed02fa71
katsulau <efthnyb.1994@gmail.com>
2019-06-12 17:44:33 +0900

    不要なモデルの削除
     - product_category
     - category
     - size
     - bland
    
    上記のモデルを削除

--- app/models/category.rb
+++
@@ -1,6 +0,0 @@
-class Category < ApplicationRecord
-  has_many   :product_categories
-  has_many   :products, through: :product_categories
-  belongs_to :parent, class_name: :Category
-  has_many   :children, class_name: :Category, foreign_key: :parent_id
-end
