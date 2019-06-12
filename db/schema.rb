f0eb315192bd8c1b18b022072774ecead3ad14d7
katsulau <efthnyb.1994@gmail.com>
2019-06-12 19:06:36 +0900

    ancestryカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_090504) do
+ActiveRecord::Schema.define(version: 2019_06_12_100513) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2019_06_12_090504) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.integer "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
