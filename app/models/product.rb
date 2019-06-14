284eb5e7633993709fda36e875acd7161d8c2602
katsulau <efthnyb.1994@gmail.com>
2019-06-14 11:51:01 +0900

    アソシエーションのコメントアウトを削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_many_attached :images
-  # belongs_to :buyer, class_name: "User"
-  # belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User"
+  belongs_to :seller, class_name: "User"
   belongs_to :category
 
   enum condition:       { 新品、未使用: 0, 未使用に近い: 1, 目立った傷や汚れなし: 2, やや傷や汚れあり: 3, 傷や汚れあり: 4, 全体的に状態が悪い: 5}
