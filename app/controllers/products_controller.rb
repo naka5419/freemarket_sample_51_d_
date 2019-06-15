741abc4a8728a352742413542fe9f280309ff0be
katsulau <efthnyb.1994@gmail.com>
2019-06-15 16:00:39 +0900

    Indexアクションの記述をリファクタリング

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,79 +5,11 @@ class ProductsController < ApplicationController
   end
 
   def index
-    ladies_ids = Category.where('ancestry LIKE(?)', "1/%").select(:id)
 
-     lady_array = []
-      ladies_ids.each do |id|
-        products = id.products
-        if products.present?
-        lady_array << products
-        end
-      end
-
-     category_ladies = []
-     lady_array.each{|products|
-       products.each{ |product|
-         category_ladies << product
-       }
-     }
-     @ladies = category_ladies.sort_by{ |a| a[:created_at] }
-
-
-    mens_ids = Category.where('ancestry LIKE(?)', "2/%").select(:id)
-
-    mens_array = []
-     mens_ids.each do |id|
-       products = id.products
-       if products.present?
-       mens_array << products
-       end
-     end
-
-    mens_category = []
-    mens_array.each{|products|
-      products.each{ |product|
-        mens_category << product
-      }
-    }
-    @mens = mens_category.sort_by{ |a| a[:created_at] }
-
-
-    kids_ids = Category.where('ancestry LIKE(?)', "3/%").select(:id)
-
-    kids_array = []
-     kids_ids.each do |id|
-       products = id.products
-       if products.present?
-       kids_array << products
-       end
-     end
-
-    kids_category = []
-    kids_array.each{|products|
-      products.each{ |product|
-        kids_category << product
-      }
-    }
-    @kids = kids_category.sort_by{ |a| a[:created_at] }
-
-    cosmetics_ids = Category.where('ancestry LIKE(?)', "7/%").select(:id)
-
-    cosmetics_array = []
-    cosmetics_ids.each do |id|
-       products = id.products
-       if products.present?
-       cosmetics_array << products
-       end
-     end
-
-    cosmetics_category = []
-    cosmetics_array.each{|products|
-      products.each{ |product|
-        cosmetics_category << product
-      }
-    }
-    @cosmetics = cosmetics_category.sort_by{ |a| a[:created_at] }
+    @ladies = Category.bring(1)
+    @mens = Category.bring(2)
+    @kids = Category.bring(3)
+    @cosmetics = Category.bring(7)
 
     @chanels = Product.where(bland: "CHANEL")
     @vuittons = Product.where(bland: "LOUIS VUITTON")
