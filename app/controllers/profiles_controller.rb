97076331b2570fc27265033b041b5fb94b9ac72e
naka5419 <hnakai323@yahoo.co.jp>
2019-06-15 14:26:12 +0900

    edit-uses-profile-show-1

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,4 +8,8 @@ class ProfilesController < ApplicationController
   def index
   end
 
+  def show
+      @product = Product.where("product.seller_id = ? and product.status = ?", "current_user.id", "0")
+  end
+
 end
