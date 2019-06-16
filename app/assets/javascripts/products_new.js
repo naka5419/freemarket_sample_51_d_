542e4b556892228959eadea6d5746742e0734d6a
katsulau <efthnyb.1994@gmail.com>
2019-06-16 19:11:52 +0900

    孫カテゴリのセレクトボックスが表示されるよう記述

--- app/assets/javascripts/products_new.js
+++ app/assets/javascripts/products_new.js
@@ -9,8 +9,17 @@ $(function() {
       $('.sell-form__detail__select-box__category').append(html);
   }
 
+  function appendChildSelect() {
+    var html = `<select class=“subcategory-form” name=“product[category_id]“ id = "child_id">
+                  <option value=“”>---</option>
+                </select>`
+
+  $('.sell-form__detail__select-box__category').append(html);
+  }
+
   $("#product_category_id").change(function(e) {
     e.preventDefault();
+    $("#select_id, #child_id").remove();
     var category_id = $(this).val();
     $.ajax({
       url: "/products/search",
@@ -28,8 +37,27 @@ $(function() {
         )
       });
     })
-    .fail(function(){
-      alert('fail');
+  })
+
+  $(document).on('change', "#select_id", function(e){
+    e.preventDefault();
+    $("#child_id").remove();
+    var childCategory_id = $(this).val();
+    $.ajax({
+      type: "GET",
+      url: "/products/search",
+      data: {childCat: childCategory_id},
+      dataType: 'json'
+    })
+    .done(function(childCategories){
+      appendChildSelect();
+      $.each(childCategories, function(index, childCategory){
+        $("#child_id").append(
+          $("<option>")
+            .val($(childCategory).attr('id'))
+            .text($(childCategory).attr('name'))
+        )
+      });
     })
   })
 });
