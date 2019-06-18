759868885a7652eca96ca3f63520de0ba47570d6
hgoya <z51.hidekazu.goya@gmail.com>
2019-06-18 21:40:07 +0900

    画像のプレビュー機能(1枚のみ)

--- app/assets/javascripts/product-image.js
+++ app/assets/javascripts/product-image.js
@@ -1,11 +1,19 @@
-// $(function() {
-//   $(".images_view__btn__delete").on('click', function() {
-//     $.ajax({
-//       url: "/product_images/1",
-//       type: "GET"
-//     })
-//     .done(function() {
-//       $(".images_view").empty();
-//     });
-//   });
-// });
+$(function(){
+  $(".sell-form__image__upload").change(function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    var preview = $(".preview");
+    reader.onload = (function(e) {
+      var html = $('<div class="preview__image"><img><div class="preview__image__btn"><a class="preview__image__btn__edit">編集</a><a class="preview__image__btn__delete">削除</a></div></div>');
+      preview.append(html);
+      preview.find('img').attr({
+        src: e.target.result,
+        height: "114px",
+        width: "116px"
+      })
+    });
+    reader.readAsDataURL(file);
+
+    $(".sell-form__image__upload").width(490);
+  });
+});
