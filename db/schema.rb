d4334e4f4d7f8ba695c05490f04f063f07eb5b1d
hgoya <z51.hidekazu.goya@gmail.com>
2019-06-18 15:41:44 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_084955) do
+ActiveRecord::Schema.define(version: 2019_06_13_063848) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
