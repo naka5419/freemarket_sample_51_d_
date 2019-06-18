70757f306a04c32ae2967641eade7bcf203178a4
RinMomii <rinmomii@gmail.com>
2019-06-18 13:28:19 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,8 +60,8 @@ set :default_env, {
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
   BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+  aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
 set :linked_files, %w{ config/master.key }
